8c3f01cd7e44283f38309887b0627359c935e8d2
六翁 <lu.hl@alibaba-inc.com>
2017-01-12 00:12:12 +0800

    up

--- docker/spark-docker/bootstrap.sh
+++ docker/spark-docker/bootstrap.sh
@@ -10,10 +10,10 @@ echo "${line// /#}"
 CMD=${1:-"exit 0"}
 if [[ "$CMD" == "master" ]]; then
   export SPARK_DAEMON_JAVA_OPTS="-Dspark.deploy.recoveryMode=ZOOKEEPER -Dspark.deploy.zookeeper.url=${ZK} -Dspark.deploy.zookeeper.dir=/spark-recovery"
-  /bin/bash -c "${SPARK_HOME}/sbin/start-master.sh -h ${SPARK_MASTER_HOST}"
+  /bin/bash -c "${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.master.Master -h ${SPARK_MASTER_HOST}"
   /bin/bash -c "tail -f ${SPARK_HOME}/logs/spark--org.apache.spark.deploy.master.Master*"
 elif [[ "$CMD" == "worker" ]]; then
-  /bin/bash -c "${SPARK_HOME}/sbin/start-slave.sh ${SPARK_MASTER_URL}"
+  /bin/bash -c "${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker ${SPARK_MASTER_URL}"
   /bin/bash -c "tail -f ${SPARK_HOME}/logs/spark--org.apache.spark.deploy.worker.Worker*"
 else
   /bin/bash -c "$*"
