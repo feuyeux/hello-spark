433a5d3338a185488c7ad57f3546b82cdd6bc893
六翁 <lu.hl@alibaba-inc.com>
2017-01-12 23:25:12 +0800

    up

--- src/main/java/org/feuyeux/bigdata/spark/rdd/HelloJavaRdd.java
+++ src/main/java/org/feuyeux/bigdata/spark/rdd/HelloJavaRdd.java
@@ -1,13 +1,15 @@
 package org.feuyeux.bigdata.spark.rdd;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
+import scala.Tuple2;
 
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
 
 /**
@@ -15,6 +17,7 @@ import java.util.regex.Pattern;
  */
 public class HelloJavaRdd {
     private static final Pattern COMMA = Pattern.compile(",");
+    private static final Pattern SPACE = Pattern.compile(" ");
     private static final String localMaster = "local[*]";
     private static final String standAloneMaster = "spark://skm1.io:7077,skm2.io:7077";
 
@@ -26,10 +29,24 @@ public class HelloJavaRdd {
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> lines = sc.textFile("file:/tmp/108").cache();
         System.out.println(lines.count());
-        JavaRDD<String> words = lines.flatMap((FlatMapFunction<String, String>) line -> {
+        JavaPairRDD<String, String> words = lines.flatMapToPair((PairFlatMapFunction<String, String, String>) line -> {
             System.out.println(line);
-            return Arrays.asList(COMMA.split(line)).iterator();
+            String[] wordsInLine = COMMA.split(line);
+            String[] names = SPACE.split(wordsInLine[2]);
+            List<Tuple2<String, String>> results = new ArrayList<>();
+            for (String word : wordsInLine) {
+                results.add(new Tuple2<>(names[0], word));
+            }
+            return results.iterator();
         });
-        System.out.println(words.count());
+
+        words.groupByKey().foreach(t -> {
+            System.out.println(t._1() + ":");
+            AtomicInteger i = new AtomicInteger(1);
+            t._2().forEach(w -> {
+                System.out.print(i.getAndIncrement() + "." + w + ",");
+            });
+            System.out.println();
+        });
     }
 }
